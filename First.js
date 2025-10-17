2af6541b49d141ebbc510f74e85d1a3341e7bd2d
studetapanacollege <mohitekaran18@gmail.com>
2025-10-17 09:43:58 +0530

    a

--- First.js
+++ First.js
@@ -1,25 +1,60 @@
-console.log("welcome vishal jadhav");
-console.log("Karan Mohite");
-console.log("I love javascript");
+// console.log("welcome vishal jadhav");
+// console.log("Karan Mohite");
+// console.log("I love javascript");
 
-age=22;
-console.log(age);
+// age=22;
+// console.log(age);
 
-fullname ="amol jadhav";
-console.log(fullname);
+// fullname ="amol jadhav";
+// console.log(fullname);
  
-price=99.99;
-console.log(price);
+// price=99.99;
+// console.log(price);
 
-x=null;
-console.log(x);
+// x=null;
+// console.log(x);
 
-y=undefined;
-console.log(y);
+// y=undefined;
+// console.log(y);
 
-isFollow=false;
-console.log(isFollow);
+// isFollow=false;
+// console.log(isFollow);
 
-fullname1="Vishal Mohite";
-FULLname1="Abinav mohite";
-console.log(fullname1);
+// fullname1="Vishal Mohite";
+// FULLname1="Abinav mohite";
+// console.log(fullname1);
+
+// fullname="Vishal Mohite";
+// _fullname="Abhinaav Mohite";
+// $fullname="Vikas sign";
+// console.log(_fullname);
+// console.log($fullname);
+// console.log(fullname);
+
+// let fullname ="Tonny";
+// let age=23;
+// let totalPrice=1000;
+// console.log(fullname);
+
+// const age=23;
+//  age=34;
+//  age=56;
+// console.log(age);
+
+
+// let a;
+// a=34;
+// console.log(a);
+
+// const a=10;
+// console.log(a);
+
+{
+    let a=5;
+    console.log(a);
+}
+{
+    let a=10;
+    console.log(a);
+}
+
