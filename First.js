6d9140b638a0b64e46af29ac9f05b30889cfe606
studetapanacollege <mohitekaran18@gmail.com>
2025-10-17 09:15:21 +0530

    a

--- First.js
+++ First.js
@@ -2,3 +2,18 @@ console.log("welcome vishal jadhav");
 console.log("Karan Mohite");
 console.log("I love javascript");
 
+age=22;
+console.log(age);
+
+fullname ="amol jadhav";
+console.log(fullname);
+ 
+price=99.99;
+console.log(price);
+
+x=null;
+console.log(x);
+
+y=undefined;
+console.log(y);
+
