a9d2a4e5a490553fc0c7d3f694f1a05675f49c3a
studetapanacollege <mohitekaran18@gmail.com>
2025-10-17 12:34:31 +0530

    initial commit

--- second.js
+++ second.js
@@ -6,13 +6,68 @@ hello world on our console window
 
 // Artimetic operator
 
-let a=5;
-let b=2;
-console.log("a= ",a,"& b=",b);
-console.log("a + b=",a+b);
-console.log("a - b=",a-b);
-console.log("a * b=",a*b);
-console.log("a / b=",a/b);
-console.log("a %b ",a%b);
-console.log("a ** b",a ** b);       
+// let a=5;
+// let b=2;
+// console.log("a= ",a,"& b=",b);
+// console.log("a + b=",a+b);
+// console.log("a - b=",a-b);
+// console.log("a * b=",a*b);
+// console.log("a / b=",a/b);
+// console.log("a %b ",a%b);
+// console.log("a ** b",a ** b);       
+
+//unary Operator
+// a=a+1;
+// a++;
+// console.log("a =",a);
+
+// a=a-1;
+// a--;
+// console.log("a =",a);
+
+// console.log("a++= ",a++);
+// console.log("a= ",a);
+
+// console.log("--a= ",--a);
+// console.log("a= ",a);
+
+// console.log("a--= ",a--);
+// console.log("a= ",a);
+
+
+// Assignment operator
+
+// let a=5;
+// let b=2;
+
+// a+=4;   //a=a+4;
+// console.log("a = ",a);
+
+// a%=4;   //a=a%4;
+// console.log("a = ",a);
+
+// a**=4;   //a=a**4;
+// console.log("a = ",a);
+
+// Comparision Operator
+// let a=5;
+// let b=2;
+// console.log("a == b",a==b);
+// console.log("a === b",a===b);
+// console.log("a > b",a>b);
+// console.log("a < b",a<b);
+// console.log("a!=b",a!=b);
+// console.log("a!==b",a!==b);
+
+// Logical Operator
+// let a=6;
+// let b=5;
+
+// let cond1=a<b;
+// let cond2=a===6;
+// console.log("cond1 && cond2",cond1 && cond2);
+// console.log("cond1 && cond2",cond1 || cond2);
+// console.log("cond1 && cond2",!(cond1));
+
+
 
