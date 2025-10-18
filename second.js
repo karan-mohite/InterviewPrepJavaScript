55019a514a90e75a74e543d2d4fa45e3102ef254
studetapanacollege <mohitekaran18@gmail.com>
2025-10-18 10:27:27 +0530

    intial commit

--- second.js
+++ second.js
@@ -159,12 +159,39 @@ const res = require("express/lib/response");
 // console.log(name);
 
 // let num=prompt("Enter a number:");
-let num=10;
-if(num%5===0)
+// let num=10;
+// if(num%5===0)
+// {
+//     console.log(num,"num is multiple of 5");
+// }
+// else
+// {
+//     console.log(num,"num is not multiple of 5");
+// }
+
+let score=55;
+let grade;
+
+if(score >=90 && score<=100)
 {
-    console.log(num,"num is multiple of 5");
+    grade="A";
 }
-else
+else if(score>=70 && score <=89)
 {
-    console.log(num,"num is not multiple of 5");
+    grade="B";
 }
+else if(score>=60 && score <=69)
+{
+    grade="C";
+}
+else if(score>=50 && score<=59)
+{
+    grade="D";
+}
+else if(score>=0 && score<=49)
+{
+    grade="F";
+}
+console.log("According to your score,your grade was:",grade);
+
+
