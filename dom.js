5490128ef821656df39b26efc0843bb6461139cc
studetapanacollege <mohitekaran18@gmail.com>
2025-10-18 13:47:16 +0530

    add

--- dom.js
+++ dom.js
@@ -1,2 +1,43 @@
-console.log("Hello");
-alert("Apana college");
+// console.log("hello");
+// window.console.log("Hello2");
+// window.alert("hello");
+   
+// console.dir(document.body.childNodes[1]);
+// console.log(document.body);
+// console.log(document.head);
+
+//  document.body.childNodes[3].innerText="abcd";
+
+// console.dir(document.body);
+
+// let button=document.getElementById("myId");
+// console.dir(button);
+
+// let headings=document.getElementsByClassName("myClass");
+// console.dir(headings);
+// console.log(headings);
+
+// let parahs=document.getElementsByTagName("p");
+// console.dir(parahs);
+
+// let firstelements=document.querySelector("p");
+// console.dir(firstelements);
+
+
+// let firstelementsAll=document.querySelectorAll("p");
+// console.dir(firstelementsAll);
+
+// let firstelements=document.querySelector(".myClass");
+// console.dir(firstelements);
+
+
+// let firstelementsAll=document.querySelectorAll(".myClass");
+// console.dir(firstelementsAll);
+
+
+// let firstelements=document.querySelector("#myId");
+// console.dir(firstelements);
+
+let firstelements=document.querySelector("p");
+console.dir(firstelements);
+   
