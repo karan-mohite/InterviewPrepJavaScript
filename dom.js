0db62b8602bc8b743eccd0795797e4943f810cc2
studetapanacollege <mohitekaran18@gmail.com>
2025-10-25 19:09:14 +0530

    .

--- dom.js
+++ dom.js
@@ -55,17 +55,47 @@
 
 // h2.innerText=h2.innerText+"From Apana college Students..!!";
 
-let divs=document.querySelectorAll(".box");
+// let divs=document.querySelectorAll(".box");
 
-let idx=1;
-for(div of divs)
-{
-    div.innerText=`new unique value ${idx}`;
-    idx++;
-}
+// let idx=1;
+// for(div of divs)
+// {
+//     div.innerText=`new unique value ${idx}`;
+//     idx++;
+// }
 // divs[0].innerText="new unique value 1";
 // divs[1].innerText="new unique value 2";
 // divs[2].innerText="new unique value 3";
 
 
+// let div=document.querySelector("div");
+// console.log(div);
 
+// let id=div.getAttribute("id");
+// console.log(id);
+
+// let name=div.getAttribute("name");
+// console.log(name);
+
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+ 
+
+// let para=document.querySelector("p");
+// console.log(para.setAttribute("class","newClass"));
+ 
+// let div=document.querySelector("div");
+// div.style.backgroundColor="purple";
+// div.style.fontSize="26px";
+// div.innerText="Hello";
+// div.style.visibility="hidden";
+
+//  let newBtn=document.createElement("button");
+//  newBtn.innerText="Click me";
+//  console.log(newBtn);
+ 
+
+//  let div=document.querySelector("p");
+//  div.after(newBtn);
+
+let newHeading=document.createElement
